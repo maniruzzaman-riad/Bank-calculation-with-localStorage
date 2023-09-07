4e2950ce9f8af61880e49910984a6dc4e8ad7592
maniruzzaman-riad <monirozzamanr94@gmail.com>
2023-09-07 15:06:20 +0600

    User validation done

--- main.js
+++ main.js
@@ -6,12 +6,59 @@ document.getElementById('login-btn').addEventListener('click',function(){
 
     passwordFild.value = '';
     emailfild.value = '';
+    loginBtn(email,password)
 
     if(email.endsWith('@gmail.com') && password.length === 6){
-        window.location.href = 'bank.html';
+        // window.location.href = 'bank.html';
     }else{
         alert('Hi user, Please Use a valid email and correct password')
     }
     
     // window.open('bank.html');
 })
+const signUp =()=>{
+    const nameField =document.getElementById('name-field');
+    const emailField =document.getElementById('email-field-create');
+    const passwordField =document.getElementById('password-fild-create');
+    const name =nameField.value
+    const email = emailField.value;
+    const password = passwordField.value;
+    nameField.value =''
+    emailField.value=''
+    passwordField.value=''
+    checkData(email,name,password)
+    // console.log(name);
+}
+
+const loginBtn =(email,password)=>{
+    const myDataString =localStorage.getItem(email)
+    const myData = JSON.parse(myDataString)
+    if(email === myData.email && password === myData.password){
+        window.location.href = 'bank.html';
+    }else{
+        alert('Please,,Use valide Email and correct password')
+    }
+    
+
+
+}
+
+const checkData=(email,name,password)=>{
+    let data ={};
+    data['email']=email;
+    data['name']=name;
+    data['password']=password;
+    const checkEmail =localStorage.getItem(email)
+    if(checkEmail){
+        alert('You have already an acount using this email')
+        // data = JSON.parse(checkEmail);
+    }else{
+        localStorage.setItem(email,JSON.stringify(data))
+    }
+    return data;
+    // console.log(email);
+}
+
+const createData=()=>{
+
+}
--- test.js
+++ test.js
@@ -1,3 +1,19 @@
-const num = 10;
-const numst = num.toString()
-console.log(numst);
+// const num = 10;
+// const numst = num.toString()
+// console.log(numst);
+
+// const name = 'Hero';
+// const age = 34;
+// const person = {name, age}; console.log(person);
+
+// const adventurer = { name: 'Alice',cat: {name: 'Lucy'} };
+// const ratName = adventurer?.rat?.name;
+// console.log(ratName);
+
+
+let arr =[];
+let obj = {};
+obj['name']='riad';
+obj['age']=27
+arr.push(obj)
+console.log(arr);
